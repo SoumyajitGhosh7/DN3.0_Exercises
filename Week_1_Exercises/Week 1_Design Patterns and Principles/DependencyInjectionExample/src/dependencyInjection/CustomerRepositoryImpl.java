f06384d2ac924f93634443166a20ea3e6f2b4136
SoumyajitGhosh7 <150497987+SoumyajitGhosh7@users.noreply.github.com>
2024-07-30 16:27:00 +0530

    Add files via upload

---
+++ Week_1_Exercises/Week 1_Design Patterns and Principles/DependencyInjectionExample/src/dependencyInjection/CustomerRepositoryImpl.java
@@ -0,0 +1,11 @@
+package dependencyInjection;
+
+public class CustomerRepositoryImpl implements CustomerRepository {
+
+	@Override
+	public Customer findCustomerById(String id) {
+		// TODO Auto-generated method stub
+		return new Customer(id, "John Doe");
+	}
+
+}
