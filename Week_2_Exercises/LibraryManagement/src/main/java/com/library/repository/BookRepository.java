fd2e138c8548df62bf65cd2f806669f57fe87341
SoumyajitGhosh7 <150497987+SoumyajitGhosh7@users.noreply.github.com>
2024-08-07 10:31:15 +0530

    Add files via upload

---
+++ Week_2_Exercises/LibraryManagement/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package com.library.repository;
+
+public class BookRepository {
+	public void testBookRepo() {
+		System.out.println("I am from BookRepository");
+	}
+}
---
+++ Week_2_Exercises/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,7 @@
+package com.library.service;
+
+public class BookService {
+	public void testBookService() {
+		System.out.println("I am from book service");
+	}
+}
