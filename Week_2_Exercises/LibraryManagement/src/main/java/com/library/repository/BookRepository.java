6ea15fbba2124d5728e7486ab6d43a9900315cb0
SoumyajitGhosh7 <150497987+SoumyajitGhosh7@users.noreply.github.com>
2024-08-08 15:17:58 +0530

    Delete Week_2_Exercises/LibraryManagement directory

--- Week_2_Exercises/LibraryManagement/src/main/java/com/library/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.library.repository;
-
-public class BookRepository {
-	public void testBookRepo() {
-		System.out.println("I am from BookRepository");
-	}
-}
--- Week_2_Exercises/LibraryManagement/src/main/java/com/library/service/BookService.java
+++
@@ -1,7 +0,0 @@
-package com.library.service;
-
-public class BookService {
-	public void testBookService() {
-		System.out.println("I am from book service");
-	}
-}
